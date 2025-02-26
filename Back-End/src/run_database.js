a68ec36692037a795eb53491165f7137410aa5d1
tianyikuang <tianyikuang@ufl.edu>
2025-02-26 16:37:59 -0500

    extended schema and examples

--- Back-End/src/run_database.js
+++ Back-End/src/run_database.js
@@ -177,6 +177,197 @@ async function setUpTestDB() {
                 maincolor : "Light Blue",
                 usage : Math.floor(Math.random() * 100),
                 photo : fs.readFileSync(photoPath + 'shorts.jpg').toString('base64')
+            },
+
+            {
+                wardrobeID: war._id,
+                name: "Red Scarf",
+                description: 'A red cozy scarf',
+                category: "scarf",
+                mainmaterial: "Cotton",
+                maincolor: "Red",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'scarf1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Outdoor Sport Socks",
+                description: 'A pair of white sports socks suits for outdoor activities',
+                category: "socks",
+                mainmaterial: "Synthetic",
+                maincolor: "White",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'sock1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Peaked Cap",
+                description: 'A hat that can be wear in leisure',
+                category: "hat",
+                mainmaterial: "Synthetic",
+                maincolor: "Brown",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'hat2.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Heart Necklace",
+                description: 'A cute heart like necklace',
+                category: "jewelry",
+                mainmaterial: "Gold",
+                maincolor: "Gold",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'jewelry1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Women's Bracelet",
+                description: 'A fancy silver bracelet suitable for women',
+                category: "jewelry",
+                mainmaterial: "Silver",
+                maincolor: "Silver",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'jewelry2.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Fancy hoody",
+                description: 'A cozy blue hoody suits for the sports',
+                category: "hoody",
+                mainmaterial: "Cotton",
+                maincolor: "Blue",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'hoody1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Black jacket",
+                description: 'A fashion jacket can be weard in winter',
+                category: "jacket",
+                mainmaterial: "Synthetic",
+                maincolor: "Black",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'jacket1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Grey sweater",
+                description: 'A sweater can be weard in winter',
+                category: "sweater",
+                mainmaterial: "Cotton",
+                maincolor: "Grey",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'sweater1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Sports singlet",
+                description: 'A singlet for running activities',
+                category: "singlet",
+                mainmaterial: "Synthetic",
+                maincolor: "Green",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'singlet1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Fashion blouse",
+                description: 'A fashion blouse for daily activities',
+                category: "blouse",
+                mainmaterial: "Cotton",
+                maincolor: "Brown",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'blouse1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Fashion jeans",
+                description: 'A jeans can be weared in casual',
+                category: "jeans",
+                mainmaterial: "Synthetic",
+                maincolor: "Blue",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'jeans1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Fashion jeans",
+                description: 'A jeans can be weared in casual',
+                category: "jeans",
+                mainmaterial: "Synthetic",
+                maincolor: "Blue",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'jeans1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "sport legging",
+                description: 'A sport legging can be used in yoga activities',
+                category: "legging",
+                mainmaterial: "Synthetic",
+                maincolor: "Black",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'legging1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "red skirt",
+                description: 'A red skirt weard in casual',
+                category: "skirt",
+                mainmaterial: "Synthetic",
+                maincolor: "Red",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'skirt1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "black belt",
+                description: 'A belt can be weard in formal places',
+                category: "belt",
+                mainmaterial: "Leather",
+                maincolor: "Black",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'belt1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "blue suit",
+                description: 'A fashion suit can be weard in formal places',
+                category: "suit",
+                mainmaterial: "fabric",
+                maincolor: "Blue",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'suit1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Trench coat",
+                description: 'A trench coat can be weard in casual places',
+                category: "coat",
+                mainmaterial: "Synthetic",
+                maincolor: "Yellow",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'coat1.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Down jacket",
+                description: 'A dark blue down jacket for winter season',
+                category: "jacket",
+                mainmaterial: "Synthetic",
+                maincolor: "Dark blue",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'jacket2.png').toString('base64')
+            },
+            {
+                wardrobeID: war._id,
+                name: "Sunflower Earrings",
+                description: 'A pair of sunflower earrings',
+                category: "jewelry",
+                mainmaterial: "Gold",
+                maincolor: "Gold",
+                usage: Math.floor(Math.random() * 100),
+                photo: fs.readFileSync(photoPath + 'jewelry3.png').toString('base64')
             }
         ]
 
